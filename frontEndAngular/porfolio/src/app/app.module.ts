d2f6e2b6e0945531fbe72111ccd644291bd0faea
vickydev92 <victoriacamblor7@gmail.com>
2023-01-22 20:16:31 -0300

    inicio de porfolio angular

---
+++ frontEndAngular/porfolio/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NavComponent } from './componentes/nav/nav.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    EncabezadoComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    SkillsComponent,
+    ProyectosComponent
+  ],
+  imports: [
+    BrowserModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
